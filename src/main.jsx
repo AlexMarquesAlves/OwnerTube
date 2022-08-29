1beb9f0b8ee508c21d4351fe947862ac2278042f
¡Duque <atanaelleonardo@gmail.com>
2022-08-28 23:32:48 -0300

    Updated favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Heading } from './components/Heading';
 import { StyledThemeProvider } from './styles/styled-theme-provider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <StyledThemeProvider>
-      <Heading>Heading</Heading>
-    </StyledThemeProvider>
-  </React.StrictMode>,
+  <StyledThemeProvider>
+    <Heading>Heading</Heading>
+  </StyledThemeProvider>,
 );
