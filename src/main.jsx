f5605763ed57fdd03b97196c551a8db35e4f53a8
¡Duque <atanaelleonardo@gmail.com>
2022-08-29 00:33:27 -0300

    Fix main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { StyledThemeProvider } from "./styles/styled-theme-provider";
 
-const root = ReactDOM.createRoot(document.getElementById({"root"}));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <StyledThemeProvider>
